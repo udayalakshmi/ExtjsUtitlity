a1b559b823793a4756ffb72b10fafdc8d3b2e40f
Udaya <udaya.lakshmi@walkingtree.tech>
2018-11-18 01:00:09 +0530

    Committing for devops

--- Ajax.js
+++ Ajax.js
@@ -6,7 +6,7 @@ Ext.define('ExtUtil.Ajax', {
       //This block will be executed before every ajax request
       });
 			Ext.Ajax.on("requestcomplete",  function(){
-				console.log('testing ad');
+				console.log('testing adjhjh');
       //This block will be executed after every ajax request
       });
 			Ext.Ajax.on("requestexception", function(){
