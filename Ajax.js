43e4c6864a011cacf9b9c006c16bf4d7bb1dbf4e
Kiran <kiran.jampani@wqalkingtree.tech>
2018-11-23 16:12:46 +0530

    rw

--- Ajax.js
+++ Ajax.js
@@ -6,7 +6,7 @@ Ext.define('ExtUtil.Ajax', {
       //This block will be executed before every ajax request
       });
 			Ext.Ajax.on("requestcomplete",  function(){
-				console.log('Test wellre test test p rerere commith');
+				console.log('Test the post receive hook');
       //This block will be executed aft wither every ajax request
       });
 			Ext.Ajax.on("requestexception", function(){
