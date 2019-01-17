17b6bb3ed268eed24a5a50b77a09f9f1cb83d39f
Kiran <kiran.jampani@wqalkingtree.tech>
2019-01-18 02:48:05 +0530

    refs #67557: test

--- Ajax.js
+++ Ajax.js
@@ -10,7 +10,7 @@ Ext.define('ExtUtil.Ajax', {
       //This block will be executed aft wither every ajax request
       });
 			Ext.Ajax.on("requestexceptifgon", function(){
-      //This block will be executed on exception of every ajax request test redmine test re dehs df updatedsd djkjehk jhwe ekw ehjwb hj
+      //This block will be executed on exception of every ajax request test redmine test re dehs df updatedsd djkjehk jhwe ekw ehjwb hj test rredmine
       });			
 		}
 	}
